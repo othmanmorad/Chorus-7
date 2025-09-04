e91beec47f6100c09951b1b7cc5f0c78fb6c7ba3
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 22:06:59 +0300

    Add files via upload

---
+++ Problem 25  Lower First Letter of Each Word.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//problem 24
+
+string ReadString()
+{
+    string text;
+    cout << "Please enter text you want\n";
+    getline(cin, text);
+    return text;
+}
+string LowerFirstLetterOfEachWord(string S1)
+{
+    bool isFirstLetter = true;
+
+
+    for (short i = 0; i < S1.length();i++)
+    {
+        if (S1[i] != ' ' && isFirstLetter)
+        {
+            S1[i] = tolower(S1[i]);
+
+
+        }
+        isFirstLetter = (S1[i] == ' ') ? true : false;
+    }
+    return S1;
+}
+int main()
+{
+    string S1 = ReadString();
+
+    cout << "\nString after conversion :\n";
+
+    S1 = LowerFirstLetterOfEachWord(S1);
+
+    cout << S1 << endl;
+
+    system("pause>0");
+}
+
